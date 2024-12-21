10908465ac205e0424fa04a6c36a14e4ba0abbf3
github-action-benchmark <github@users.noreply.github.com>
2024-12-21 20:48:29 +0000

    add Benchmark (cargo) benchmark result for bac39ea6f4f25ea3833f2e3006046e16870c6fb8

---
+++ compiler/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1734814109085,
+  "repoUrl": "https://github.com/414owen/outlines-core",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "414owen",
+            "username": "414owen"
+          },
+          "committer": {
+            "name": "414owen",
+            "username": "414owen"
+          },
+          "id": "bac39ea6f4f25ea3833f2e3006046e16870c6fb8",
+          "message": "Test a regression",
+          "timestamp": "2024-12-21T17:26:13Z",
+          "url": "https://github.com/414owen/outlines-core/pull/2/commits/bac39ea6f4f25ea3833f2e3006046e16870c6fb8"
+        },
+        "date": 1734814108660,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 53511,
+            "range": "± 226",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
