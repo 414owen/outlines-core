80feeea5582f74fa1c87577f0402e5bb27d2cfac
Owen Shepherd <owen@owen.cafe>
2024-12-22 00:02:52 +0000

    Update iters

--- benchmarks/criterion.rs
+++ benchmarks/criterion.rs
@@ -10,7 +10,7 @@ fn fibonacci(n: u64) -> u64 {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(15))));
+    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(17))));
 }
 
 criterion_group!(benches, criterion_benchmark);
