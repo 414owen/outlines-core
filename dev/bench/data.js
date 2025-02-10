5387eab01ef0713b9667418d4f03ba55cab69605
github-action-benchmark <github@users.noreply.github.com>
2025-02-10 02:39:57 +0000

    add Benchmark (cargo) benchmark result for 60c79629ceae30602cd79fed0ef1129f9e2a90a8

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1739155197687,
+  "repoUrl": "https://github.com/414owen/outlines-core",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "owen@owen.cafe",
+            "name": "Owen Shepherd",
+            "username": "414owen"
+          },
+          "committer": {
+            "email": "owen@owen.cafe",
+            "name": "Owen Shepherd",
+            "username": "414owen"
+          },
+          "distinct": true,
+          "id": "60c79629ceae30602cd79fed0ef1129f9e2a90a8",
+          "message": "Remove all the things...",
+          "timestamp": "2025-02-10T02:38:22Z",
+          "tree_id": "337da8fe45be818979a177bf912383d5feb712ea",
+          "url": "https://github.com/414owen/outlines-core/commit/60c79629ceae30602cd79fed0ef1129f9e2a90a8"
+        },
+        "date": 1739155197297,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 1726,
+            "range": "± 51",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
