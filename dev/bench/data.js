2fa70fbfabcf9ac02f44d308d3f099c4db763368
github-action-benchmark <github@users.noreply.github.com>
2025-02-10 02:43:33 +0000

    add Benchmark (cargo) benchmark result for 722d145387824b06b57a2adc334b6db130088c0a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739155197687,
+  "lastUpdate": 1739155412989,
   "repoUrl": "https://github.com/414owen/outlines-core",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "fib 20",
             "value": 1726,
             "range": "± 51",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "owen@owen.cafe",
+            "name": "Owen Shepherd",
+            "username": "414owen"
+          },
+          "committer": {
+            "email": "owen@owen.cafe",
+            "name": "Owen Shepherd",
+            "username": "414owen"
+          },
+          "distinct": true,
+          "id": "722d145387824b06b57a2adc334b6db130088c0a",
+          "message": "test",
+          "timestamp": "2025-02-10T02:41:49Z",
+          "tree_id": "337da8fe45be818979a177bf912383d5feb712ea",
+          "url": "https://github.com/414owen/outlines-core/commit/722d145387824b06b57a2adc334b6db130088c0a"
+        },
+        "date": 1739155412371,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 1726,
+            "range": "± 49",
             "unit": "ns/iter"
           }
         ]
